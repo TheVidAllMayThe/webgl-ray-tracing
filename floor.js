41b25ef8ee3b7f14d64bf3aa2592a8878f866144
David Almeida <davidmoreiraalmeida@hotmail.com>
2018-11-14 14:42:34 +0000

    full floor

--- floor.js
+++ floor.js
@@ -4,44 +4,27 @@ floor.rotYYOn = false
 floor.rotZZOn = false
 floor.sx = floor.sy = floor.sz = 1.0
 
-/*
-for(let i = -25; i< 25; i++){
-    tmpsq = new simpleSquareModel()
-    for(let indx = 0; indx < tmpsq.vertices.length/3; indx++){
-        tmpsq.vertices[indx*3] *= 0.24
-        tmpsq.vertices[indx*3 + 1] *= 0.24
-        tmpsq.vertices[indx*3 + 2] *= 0.24
-        //tmpsq.vertices[indx*3] += k * 0.5
-        tmpsq.vertices[indx*3 + 1] += -1.0
-        tmpsq.vertices[indx*3 + 2] += i * 0.5
-        
-        tmpsq.colors[indx*3] = tmpsq.colors[indx*3 + 1] = tmpsq.colors[indx*3 + 2] = (i%2) ? 0.0 : 0.5 
-    }
-    floor.vertices = floor.vertices.concat(tmpsq.vertices)
-    floor.colors = floor.colors.concat(tmpsq.colors)
-
-}
-*/
 
 for(let i = -25; i< 25; i++){
     for(let k = -25; k< 25; k++){
         tmpsq = new simpleSquareModel()
         for(let indx = 0; indx < tmpsq.vertices.length/3; indx++){
-            tmpsq.vertices[indx*3] *= 0.24
-            tmpsq.vertices[indx*3 + 1] *= 0.24
-            tmpsq.vertices[indx*3 + 2] *= 0.24
+            tmpsq.vertices[indx*3] *= 0.25
+            tmpsq.vertices[indx*3 + 1] *= 0.25
+            tmpsq.vertices[indx*3 + 2] *= 0.25
             tmpsq.vertices[indx*3] += k * 0.5
             tmpsq.vertices[indx*3 + 1] += -1.0
             tmpsq.vertices[indx*3 + 2] += i * 0.5
             
-            tmpsq.colors[indx*3] = tmpsq.colors[indx*3 + 1] = tmpsq.colors[indx*3 + 2] = (i%2) ? 0.0 : 0.5 
+            tmpi = i + k
+            tmpsq.colors[indx*3    ] = (tmpi%2) ? 0.0 : 0.5 
+            tmpsq.colors[indx*3 + 1] = (tmpi%2) ? 0.0 : 0.5 
+            tmpsq.colors[indx*3 + 2] = (tmpi%2) ? 0.0 : 0.5 
         }
         floor.vertices = floor.vertices.concat(tmpsq.vertices)
         floor.colors = floor.colors.concat(tmpsq.colors)
     }
 }
-
-console.log(floor.vertices)
 
 computeVertexNormals( floor.vertices, floor.normals)
 sceneModels.push(floor)
